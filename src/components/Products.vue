ac701dadc1267896da05367f7876034c19c21504
Flavio <flavio.origgi@outlook.it>
2021-07-06 16:03:50 +0200

    terminata fascia azzurra

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,23 +3,23 @@
     <div class="container">
       <div class="icon-contenent">
         <img class="img-products" src="../assets/buy-comics-digital-comics.png" alt="">
-        <h5>Digital Comics</h5>
+        <p class="text-icon-products">digital Comics</p>
       </div>
       <div class="icon-contenent">
         <img class="img-products" src="../assets/buy-comics-merchandise.png" alt="">
-        <h5></h5>
+        <p class="text-icon-products">dc merchandise</p>
       </div>
       <div class="icon-contenent">
         <img class="img-products" src="../assets/buy-comics-subscriptions.png" alt="">
-        <h5></h5>
+        <p class="text-icon-products">subscriptions</p>
       </div>
       <div class="icon-contenent">
-        <img class="img-products" src="../assets/buy-comics-shop-locator.png" alt="">
-        <h5></h5>
+        <img class="img-locator-products" src="../assets/buy-comics-shop-locator.png" alt="">
+        <p class="text-icon-products">comics shop locator</p>
       </div>
       <div class="icon-contenent">
-        <img class="img-products" src="../assets/buy-dc-power-visa.svg" alt="">
-        <h5></h5>
+        <img class="img-visa-products" src="../assets/buy-dc-power-visa.svg" alt="">
+        <p class="text-icon-products">dc power visa</p>
       </div>
       
 
@@ -55,9 +55,29 @@ export default {
 
     .icon-contenent{
       display: flex;
+      width: 20%;
 
       .img-products{
-        width: 5%;
+        width: 20%;
+        margin-right: 5px;
+      }
+
+      .text-icon-products{
+        color: white;
+        text-transform:uppercase;
+        font-size: 16px;
+        padding: 20px 0;       
+      }
+
+      .img-locator-products{
+        width: 15%;
+        margin-right: 5px;
+      }
+
+      .img-visa-products{   
+        width: 70px;
+        margin-right: 5px;
+
       }
 
     }
